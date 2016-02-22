23394782053bc721982f39e5eaa3fdbffc5d9b25
sonnymyet <shm6@sfu.ca>
2016-02-21 18:41:31 -0800

    Project Initialized

---
+++ Source/PullProjectGit/PullProjectGitGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PullProjectGitGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PULLPROJECTGIT_API APullProjectGitGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
